baa93b094c74d02acb40056ab0e297b5c11893f1
Woolgar <tn116@hotmail.com>
2024-06-29 16:49:20 +0200

    remove studio plz

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["@/assets/main.scss"],
-  modules: ["@vueuse/nuxt", "@nuxt/content", "@nuxthq/studio", "@nuxt/image"],
+  modules: ["@vueuse/nuxt", "@nuxt/content", "@nuxt/image"],
 
   runtimeConfig: { 
 
