ff84c97b7ae7f55c32e38521bf631eb917d9e0dd
Woolgar <tn116@hotmail.com>
2024-08-18 18:40:01 +0200

    test plausible

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,3 +1,20 @@
 <template>
   <NuxtPage />
 </template>
+
+<script>
+  onNuxtReady(() => {
+    useHead({
+      script: [
+        {
+          id: 'plausible',
+          src: 'https://plausible-kk8cscg0c0cgcgw8wgoc0sck.woolgar.dev/js/script.js',
+          type: 'text/javascript',
+          tagPosition: 'bodyClose',
+          dataDomain: 'woolgar.dev',
+          defer:true,
+        },
+      ],
+    });
+  });
+</script>
